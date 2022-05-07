5c73a9f38dfef522d2b6e5dc2ea301c26803e95c
- <-@DESKTOP-TSQV90L>
2022-05-07 20:34:50 +0300

    Добавьте файлы проекта.

---
+++ лаб 7/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лаб_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] Mas = new int[20];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random rand = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                Mas[i] = rand.Next(-50, 50);
+                listBox1.Items.Add("Mas[" + i.ToString() +
+     "] = " + Mas[i].ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int max = 0, j = -1;
+            listBox2.Items.Clear();
+            for (int i = 0; i < 20; i++)
+            {
+                if (Mas[i] > max)
+                {
+                    max = Mas[i];
+                    j = i;
+                }
+            }
+
+            Mas[j] = Mas[0];
+            Mas[0] = max;
+
+            for (int i = 0; i < 20; i++)
+            {
+                listBox2.Items.Add("Mas[" + Convert.ToString(i)
+                + "] = " + Mas[i].ToString());
+            }
+        }
+
+    }
+}
+
---
+++ лаб 7/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("лаб 7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("лаб 7")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e17d4f6b-884b-4661-a0bb-217c6ff97f95")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
